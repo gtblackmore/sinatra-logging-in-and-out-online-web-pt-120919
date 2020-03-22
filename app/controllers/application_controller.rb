a0e95c4542b0d7191ff641ec2cd3d0d1578238d9
George Blackmore <gtblackmore@gmail.com>
2020-03-22 21:47:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
     erb :account
   end
 
