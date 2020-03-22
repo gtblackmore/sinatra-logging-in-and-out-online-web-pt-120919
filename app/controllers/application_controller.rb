58395515e42ab9bf689e15b9102e4c3b4b65cdd3
George Blackmore <gtblackmore@gmail.com>
2020-03-22 22:01:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @users != nil && params[:password] == @user.password
+    if @user != nil && params[:password] == @user.password
       session[:user_id] = @user.id
       redirect to '/account'
     else
