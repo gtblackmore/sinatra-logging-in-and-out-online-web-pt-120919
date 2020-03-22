952f95497ad1a809d265587af0a98892b6ac1779
George Blackmore <gtblackmore@gmail.com>
2020-03-22 21:43:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @users ==
+    if @users != nil && params[:password]
   end
 
   get '/account' do
